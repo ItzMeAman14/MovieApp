df16fc647ec697a8d721b3a3f6e63a94f4cac181
ItzMeAman14 <amanrehman2020@gmail.com>
2024-10-03 17:01:13 +0530

    Search Added

--- src/main/java/com/movieapp/demo/Controller/movieController.java
+++ src/main/java/com/movieapp/demo/Controller/movieController.java
@@ -9,14 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class movieController {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    private final String UPLOAD_DIR = "src/main/resources/static/images/";
 
     @GetMapping("/")
     public String index(Model model, @RequestParam(required = false) String search) {
@@ -40,22 +49,42 @@ public class movieController {
 
     @PostMapping("/addMovie")
     public String addMovie(@RequestParam String title,
-                           @RequestParam String poster,
+                           @RequestParam MultipartFile poster,
                            @RequestParam String description) {
         movie obj = new movie();
         obj.setTitle(title);
-        obj.setPoster(poster);
+        
+        if (!poster.isEmpty()) {
+            try {
+                File dir = new File(UPLOAD_DIR);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                
+                String filePath = UPLOAD_DIR + poster.getOriginalFilename();
+                Path path = Paths.get(filePath);
+                Files.write(path, poster.getBytes());
+
+                obj.setPoster("images/" + poster.getOriginalFilename());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
         obj.setDescription(description);
         movieRepository.save(obj);
         return "redirect:/";
     }
 
-
     @GetMapping("/movie/{id}")
     public String getMovieDetails(@PathVariable Long id, Model model) {
-        movie movie = movieRepository.findById(id).orElse(null);
-        model.addAttribute("movie", movie);
-        return "movieDetail"; // Create a movieDetails.html file in src/main/resources/templates
+        Optional<movie> movieDetail = movieRepository.findById(id);
+        
+        if (movieDetail.isPresent()) {
+            model.addAttribute("movie", movieDetail.get());
+            return "movieDetail";
+        } else {
+            return "error";
+        }
     }
-
 }
